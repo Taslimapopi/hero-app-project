812c34859e96bdfaef9784912710b20344e78324
taslima <garryanderson337@gmail.com>
2025-10-08 20:28:34 +0600

    3rd commit

--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -1,27 +1,41 @@
-import React from "react";
 import { useParams } from "react-router";
 import useApps from "../Hooks/useApps";
 
 const AppDetails = () => {
   const { id } = useParams();
-  const { apps,loading } = useApps();
+  const { apps, loading } = useApps();
 
   const app = apps.find((p) => String(p.id) === id);
- if (loading) return <p>loading....</p>
-  const{title,image,companyName,description,reviews,downloads,ratingAvg,size} = app || {};
+  if (loading) return <p>loadi ng....</p>;
+  const {
+    title,
+    image,
+    companyName,
+    description,
+    reviews,
+    downloads,
+    ratingAvg,
+    size,
+  } = app || {};
 
-  console.log(app);
+  const handleInstalledApp = () => {
+    const existingApps = JSON.parse(localStorage.getItem("installedApp"));
+    let updatedApps = [];
+    if (existingApps) {
+      const isDuplicate = existingApps.some((p) => p.id === app.id);
+      if (isDuplicate) return alert("already installed");
+      updatedApps = [...existingApps, app];
+    } else{
+       updatedApps.push(app);
+    }
+    localStorage.setItem("installedApp", JSON.stringify(updatedApps));
+  };
 
-  console.log(id);
   return (
     <div>
       <div className="card card-side bg-base-100 shadow-sm">
         <figure>
-          <img
-            className="w-50 h-50"
-            src={image}
-            alt="Movie"
-          />
+          <img className="w-50 h-50" src={image} alt="Movie" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
@@ -32,13 +46,16 @@ const AppDetails = () => {
             <p>{reviews}</p>
           </div>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Installed Now ({size}MB)</button>
+            <button className="btn btn-primary" onClick={handleInstalledApp}>
+              Installed Now ({size}MB)
+            </button>
           </div>
         </div>
       </div>
-      <div><h1 className="text-center font-semibold p-5">Description:</h1> 
+      <div>
+        <h1 className="text-center font-semibold p-5">Description:</h1>
         <p className="text-center">{description}</p>
-        </div>
+      </div>
     </div>
   );
 };
